298daa36c04e0d934155f674e713d171975abe79
Robert Koziolek <robertkoziolek4c@gmail.com>
2018-11-21 20:36:53 +0100

    First commit

---
+++ src/main/java/com/robcio/SpringStuffThymeleaf/SpringStuffThymeleafApplication.java
@@ -0,0 +1,12 @@
+package com.robcio.SpringStuffThymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStuffThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringStuffThymeleafApplication.class, args);
+	}
+}
---
+++ src/test/java/com/robcio/springstuffthymeleaf/SpringStuffThymeleaf/SpringStuffThymeleafApplicationTests.java
@@ -0,0 +1,16 @@
+package com.robcio.springstuffthymeleaf.SpringStuffThymeleaf;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringStuffThymeleafApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
